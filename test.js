4fc7c5c685286d59ce28e4a3f7acf6f2899cd6c9
Hend Tarek Isleem <53368022+hend-isleem@users.noreply.github.com>
2019-12-01 11:23:02 +0200

    Create test.js

---
+++ test.js
@@ -0,0 +1,17 @@
+const { test1, func2 } = require("./unit.js");
+
+describe("Sample Test", () => {
+  it("should test that true === true", () => {
+    expect(true).toBe(true);
+  });
+});
+
+test("return haha <name>", () => {
+  var t = test1("really? -_-");
+  expect(t).toBe("haha really? -_-");
+});
+
+test("return num1+num2  * num3", () => {
+    var result = func2(1, 1, 2);
+    expect(result).toBe(4);
+});
