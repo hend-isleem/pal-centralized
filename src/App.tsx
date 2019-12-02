92a5413e7ff4c7230722636b691a8b38343f7ddf
mohammad-khamaiseh-2 <m.khamaiseh2@student.aaup.edu>
2019-12-02 10:37:47 +0200

    Component folder structure and empty Sign Up

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import "semantic-ui-css/semantic.min.css";
+import SignUp from "./components/general/Sign-up";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <h1>Hello .. </h1>
+      <SignUp />
     </div>
   );
 };
