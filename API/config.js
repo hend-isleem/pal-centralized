cf39a25a3dcba0398bef2bd90ac8260c173b6c15
hend-isleem <hendisleem@gmail.com>
2019-12-10 13:21:42 +0200

    sending mails

---
+++ API/config.js
@@ -0,0 +1,7 @@
+module.exports={
+    APP_ID: '6dfce3b7' || process.env.APP_ID,
+    API_KEY: '9e3f98e6d63066a68e754a235b581e99' || process.env.API_KEY,
+    BASE_URL:"https://https:/api.adzuna.com/v1/api/jobs",
+    BASE_PARAMS:"searc/1?&results_per_page=20&content_type=application/json"
+
+}
