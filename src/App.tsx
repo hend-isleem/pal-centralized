4d815252982ec4c1d9985d7c92bc45c0cc169f50
mohammad-khamaiseh-2 <m.khamaiseh2@student.aaup.edu>
2019-12-15 15:11:41 +0200

    to pull

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "semantic-ui-css/semantic.min.css";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import { useSelector, useDispatch } from "react-redux";
 import {
   PrivateRouteLogin,
